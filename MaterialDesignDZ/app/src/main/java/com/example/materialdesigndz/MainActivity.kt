5b141e2c4e0352032ebd306e52e0b6e9621398ee
SergSerg <sergio.ne@inbox.ru>
2020-11-12 20:47:12 +0600

    Применил атрибут app:errorEnabled = "true"

--- MaterialDesignDZ/app/src/main/java/com/example/materialdesigndz/MainActivity.kt
+++ MaterialDesignDZ/app/src/main/java/com/example/materialdesigndz/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.materialdesigndz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
+
 
 class MainActivity : AppCompatActivity() {
+    private var nTextInputLayout: TextInputLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nTextInputLayout = findViewById(R.id.name_text_input_layout)
+        showError()
+
+
+    }
+
+    private fun showError() {
+       nTextInputLayout?.error = "Error input"
     }
 }
